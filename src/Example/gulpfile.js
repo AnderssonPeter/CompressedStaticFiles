c04e3a69d601da2b0bc62b33ec8e45cfc76744b0
Peter Andersson <petoj87@gmail.com>
2016-07-16 00:53:11 +0200

    Added Example project

---
+++ src/Example/gulpfile.js
@@ -0,0 +1,28 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp'),
+    rename = require('gulp-rename'),
+    brotli = require('gulp-brotli'),
+    zopfli = require('gulp-zopfli')
+
+var paths = ['wwwroot/**/*.js',
+             'wwwroot/**/*.html',
+             'wwwroot/**/*.htm',
+             'wwwroot/**/*.css',
+             'wwwroot/**/*.svg',
+             'wwwroot/**/*.ico'];
+
+gulp.task('gzip', function () {
+    return gulp.src(paths)
+               .pipe(zopfli())
+               .pipe(gulp.dest('wwwroot'));
+});
+
+gulp.task('brotli', function () {
+    return gulp.src(paths)
+               .pipe(brotli.compress({ skipLarger: true, quality: 11 }))
+               .pipe(gulp.dest('wwwroot'));
+});
