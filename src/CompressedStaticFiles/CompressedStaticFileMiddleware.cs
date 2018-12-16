e358eb2a16b5589bcaf22954f4b15b163e8674ea
Mathias Raacke <mathias@raacke.info>
2018-12-16 12:41:09 +0100

    feat: added support for file providers
    The middleware now uses the file provider that is configured in the StaticFileOptions instead of always using the ContentRootFileProvider of IHostingEnvironment.

--- src/CompressedStaticFiles/CompressedStaticFileMiddleware.cs
+++ src/CompressedStaticFiles/CompressedStaticFileMiddleware.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
@@ -14,23 +15,22 @@ namespace CompressedStaticFiles
 {
     public class CompressedStaticFileMiddleware
     {
-        private static Dictionary<string, string> compressionTypes = new Dictionary<string, string>()
-        {{"gzip", ".gz" },
-         {"br", ".br" }};
-
-        private readonly IHostingEnvironment _hostingEnv;
+        private static Dictionary<string, string> compressionTypes = 
+            new Dictionary<string, string>()
+            {
+                { "gzip", ".gz" }, {"br", ".br" }
+            };
+        
+        private readonly IOptions<StaticFileOptions> _staticFileOptions;
         private readonly StaticFileMiddleware _base;
         private readonly ILogger _logger;
-        public CompressedStaticFileMiddleware(RequestDelegate next, IHostingEnvironment hostingEnv, IOptions<StaticFileOptions> staticFileOptions, ILoggerFactory loggerFactory)
+
+        public CompressedStaticFileMiddleware(
+            RequestDelegate next, IHostingEnvironment hostingEnv, IOptions<StaticFileOptions> staticFileOptions, ILoggerFactory loggerFactory)
         {
             if (next == null)
             {
                 throw new ArgumentNullException(nameof(next));
-            }
-
-            if (staticFileOptions == null)
-            {
-                throw new ArgumentNullException(nameof(staticFileOptions));
             }
 
             if (loggerFactory == null)
@@ -38,17 +38,29 @@ namespace CompressedStaticFiles
                 throw new ArgumentNullException(nameof(loggerFactory));
             }
 
-            _hostingEnv = hostingEnv ?? throw new ArgumentNullException(nameof(hostingEnv));
+            if (hostingEnv == null)
+            {
+                throw new ArgumentNullException(nameof(hostingEnv));
+            }
+
             _logger = loggerFactory.CreateLogger<CompressedStaticFileMiddleware>();
+
+
+            this._staticFileOptions = staticFileOptions ?? throw new ArgumentNullException(nameof(staticFileOptions));
+            InitializeStaticFileOptions(hostingEnv, staticFileOptions);
+
+            _base = new StaticFileMiddleware(next, hostingEnv, staticFileOptions, loggerFactory);
+        }
+
+        private static void InitializeStaticFileOptions(IHostingEnvironment hostingEnv, IOptions<StaticFileOptions> staticFileOptions)
+        {
+            staticFileOptions.Value.FileProvider = staticFileOptions.Value.FileProvider ?? hostingEnv.WebRootFileProvider;
             var contentTypeProvider = staticFileOptions.Value.ContentTypeProvider ?? new FileExtensionContentTypeProvider();
-            var fileExtensionContentTypeProvider = contentTypeProvider as FileExtensionContentTypeProvider;
-            if (fileExtensionContentTypeProvider != null)
+            if (contentTypeProvider is FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
             {
                 fileExtensionContentTypeProvider.Mappings[".br"] = "application/brotli";
             }
-
             staticFileOptions.Value.ContentTypeProvider = contentTypeProvider;
-            staticFileOptions.Value.FileProvider = staticFileOptions.Value.FileProvider ?? hostingEnv.WebRootFileProvider;
 
             var originalPrepareResponse = staticFileOptions.Value.OnPrepareResponse;
             staticFileOptions.Value.OnPrepareResponse = ctx =>
@@ -66,51 +78,47 @@ namespace CompressedStaticFiles
                     }
                 }
             };
-
-            _base = new StaticFileMiddleware(next, hostingEnv, staticFileOptions, loggerFactory);
         }
 
         public Task Invoke(HttpContext context)
         {
             if (context.Request.Path.HasValue)
             {
-                string acceptEncoding = context.Request.Headers["Accept-Encoding"];
-                FileInfo matchedFile = null;
-                string[] browserSupportedCompressionTypes = context.Request.Headers["Accept-Encoding"].ToString().Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                var orginalFilePath = Path.Combine(
-                                _hostingEnv.WebRootPath, context.Request.Path.Value.StartsWith("/")
-                                ? context.Request.Path.Value.Remove(0, 1)
-                                : context.Request.Path.Value
-                            );
-                var orginalFile = new FileInfo(orginalFilePath);
-                if (orginalFile.Exists)
-                {
-                    foreach (var compressionType in compressionTypes.Keys)
-                    {
-                        if (browserSupportedCompressionTypes.Contains(compressionType, StringComparer.OrdinalIgnoreCase))
-                        {
-                            var fileExtension = compressionTypes[compressionType];
-                            var filePath = orginalFilePath + fileExtension;
-                            var file = new FileInfo(filePath);
-                            if (file.Exists && file.Length < orginalFile.Length)
-                            {
-                                if (matchedFile == null)
-                                    matchedFile = file;
-                                else if (matchedFile.Length > file.Length)
-                                    matchedFile = file;
-                            }
-                        }
-                    }
-                    if (matchedFile != null)
-                    {
-                        var matchedPath = context.Request.Path.Value + matchedFile.Extension;
-                        _logger.LogFileServed(context.Request.Path.Value, matchedPath, orginalFile.Length, matchedFile.Length);
-                        context.Request.Path = new PathString(matchedPath);
-                        return _base.Invoke(context);
-                    }
-                }
+                ProcessRequest(context);
             }
             return _base.Invoke(context);
+        }
+
+        private void ProcessRequest(HttpContext context)
+        {
+            string acceptEncoding = context.Request.Headers["Accept-Encoding"];
+            string[] browserSupportedCompressionTypes = context.Request.Headers["Accept-Encoding"].Select(s => s).ToArray();
+
+            var fileSystem = _staticFileOptions.Value.FileProvider;
+            var originalFile = fileSystem.GetFileInfo(context.Request.Path);
+
+            if (!originalFile.Exists)
+            {
+                return;
+            }
+
+            IFileInfo matchedFile = originalFile;
+            foreach (var compressionType in compressionTypes.Keys.Intersect(browserSupportedCompressionTypes, StringComparer.OrdinalIgnoreCase))                
+            {                
+                var fileExtension = compressionTypes[compressionType];
+                var file = fileSystem.GetFileInfo(context.Request.Path + fileExtension);
+                if (file.Exists && file.Length < matchedFile.Length)
+                {                        
+                    matchedFile = file;
+                }                
+            }
+
+            if (matchedFile != originalFile)
+            {
+                var matchedPath = context.Request.Path.Value + Path.GetExtension(matchedFile.Name);
+                _logger.LogFileServed(context.Request.Path.Value, matchedPath, originalFile.Length, matchedFile.Length);
+                context.Request.Path = new PathString(matchedPath);
+            }            
         }
     }
 }
